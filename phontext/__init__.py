5974d220fa5a4144b7dd5b3285f8a9b8d8dcacd5
rolandomunoz <rolando.muar@gmail.com>
2020-06-25 06:06:22 -0500

    Added new module

--- phontext/__init__.py
+++ phontext/__init__.py
@@ -1,2 +1,4 @@
-from .parser import *
-from .fimport import *
+from .read_files import ReadFile
+from .parser import PhonParser
+from .word import Word
+from .corpus import Corpus
---
+++ phontext/corpus.py
@@ -0,0 +1,16 @@
+# Written by Rolando Muñoz (2019-2020)
+try:
+  from .word import Word
+  from .read_files import ReadFile
+except:
+  from word import Word
+  from .read_files import ReadFile
+  
+class Corpus():
+  pass
+  # Search segments
+  
+  # Find Minimal pairs
+  
+if __name__ == '__main__':
+  pass
