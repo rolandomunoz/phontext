225c129a34790aef96ee89fa1299e8d14459471b
rolandomunoz <rolando.muar@gmail.com>
2022-08-29 12:03:19 -0500

    feat: Test with unittest

--- tests/test_word.py
+++ tests/test_word.py
@@ -1,37 +1,39 @@
 import sys
+import unittest
 import os
 sys.path.insert(0, os.path.abspath('../src'))
 from phontext import Word
 
+config = {
+    'vowels': ['a', 'e', 'i', 'o', 'u'],
+    'consonants': ['ch', 'ts', 'rr']
+}
+
+class TestWordMethods(unittest.TestCase):
+
+    def test_attributes(self):
+        word = Word('chincha', **config)
+        self.assertEqual(len(word), 5)
+        self.assertEqual(word.cv, "CVCCV")
+        self.assertEqual(word.word_str, "ch i n ch a")
+        self.assertEqual(word.word, ["ch", "i","n", "ch", "a"])
+
+    def test_operators(self):
+        word1 = Word('paso', **config)
+        word2 = Word(['p','a', 's','o'], **config)
+        word3 = Word('peso', **config)
+
+        self.assertTrue(word1==word2)
+        self.assertFalse(word1==word3)
+
+    def test_minimal_pairs(self):
+        word1 = Word('tasa', **config)
+        word2 = Word('kasa', **config)
+        word3 = Word('tasl', **config)
+
+        self.assertTrue(word1.is_minimal_pair(word2))
+        self.assertFalse(word1.is_minimal_pair(word3))
+
 if __name__ == '__main__':
+    unittest.main()
 
-    config = {
-        'vowels': ['a', 'e', 'i', 'o', 'u'],
-        'consonants': ['ch', 'ts', 'rr']
-    }
-
-    word1 = Word('chincha', **config)
-    word2 = Word('chinchaa', **config)
-    word3 = Word(['p', 'e', 'rr', 'o'], **config)
-
-
-    a = word3.get_invalid_segments()
-    print(a)
-
-    print(word1)
-    print(word2)
-    print(word3)
-    word3.reverse()
-    print(word3)
-
-    # Check minimal pairs
-    # This must be True
-    word4 = Word('paso', **config)
-    word5 = Word('peso', **config)
-    word6 = Word('kasa', **config)
-
-    # This must be False
-    word7 = Word('tasa', **config)
-    word8 = Word('tasl', **config)
-    result = word7.is_minimal_pair(word8)
-    print(result)
