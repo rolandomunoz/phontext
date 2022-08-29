2c6fc7a56d3ab9065a5b179b1d269227b8aabb44
rolandomunoz <rolando.muar@gmail.com>
2022-08-29 00:47:02 -0500

    Add tests

---
+++ tests/test_corpus.py
@@ -0,0 +1,18 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath('..'))
+from phontext import Corpus
+
+if __name__ == '__main__':
+
+    fin = r'C:\Users\rolan\Documents\tesis-yessica\datos_procesados\wordlist-bora.txt'
+    fout = r'C:\Users\rolan\Documents\tesis-yessica\datos_procesados\análisis.txt'
+
+    config = {
+        'vowels': ['a', 'e', 'i', 'o', 'u', 'ɨ', 'ɨɨ', 'ee'],
+        'consonants': ['ʔ', 'ts', 'm', 'pj', 'ɲ', 'kʷ', 'ʧ', 'kw', 'ɻ', 'ɲ']
+    }
+
+    corpus = Corpus(**config)
+    corpus.read_from_text_file(fin)
+    corpus.write(fout)
---
+++ tests/test_word.py
@@ -0,0 +1,37 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath('..'))
+from phontext import Word
+
+if __name__ == '__main__':
+
+    config = {
+        'vowels': ['a', 'e', 'i', 'o', 'u'],
+        'consonants': ['ch', 'ts', 'rr']
+    }
+
+    word1 = Word('chincha', **config)
+    word2 = Word('chinchaa', **config)
+    word3 = Word(['p', 'e', 'rr', 'o'], **config)
+
+
+    a = word3.get_invalid_segments()
+    print(a)
+
+    print(word1)
+    print(word2)
+    print(word3)
+    word3.reverse()
+    print(word3)
+
+    # Check minimal pairs
+    # This must be True
+    word4 = Word('paso', **config)
+    word5 = Word('peso', **config)
+    word6 = Word('kasa', **config)
+
+    # This must be False
+    word7 = Word('tasa', **config)
+    word8 = Word('tasl', **config)
+    result = word7.is_minimal_pair(word8)
+    print(result)
